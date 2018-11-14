4f04b7dc41c204a4732f3ea6e8b290d6f5646bc4
Mai Tran <maitran215@mac.com>
2018-11-14 22:17:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
-def fizzbuzz(int)
-  if (int % 3 == 0)
+def fizzbuzz(i)
+  if (i % 3 == 0)
     puts "Fizz"
-  elsif (int % 5 == 0)
+  elsif (i % 5 == 0)
     puts "Buzz"
-  elsif (int % 5 == 0) and (int % 3 == 0)
+  elsif (i % 5 == 0) && (i % 3 == 0)
     puts "FizzBuzz"
   else
     puts "nil"
