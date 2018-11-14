050298797d35ff96f86a868437da550b54641dcc
Mai Tran <maitran215@mac.com>
2018-11-14 22:17:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(int)
-  if int % 3 == 0
+  if (int % 3 == 0)
     puts "Fizz"
-  elsif int % 5 == 0
+  elsif (int % 5 == 0)
     puts "Buzz"
   elsif (int % 5 == 0) && (int % 3 == 0)
     puts "FizzBuzz"
